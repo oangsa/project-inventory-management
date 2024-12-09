a53be1f1e765c1d8ffe10856f78b233397c539a3
oangsa <sukruangkul.aongsa@gmail.com>
2024-12-10 05:32:54 +0700

    [Update] Update prismadb.ts

--- libs/prismadb.ts
+++ libs/prismadb.ts
@@ -1,13 +1,10 @@
 import { PrismaClient } from "@prisma/client"
-import { withOptimize } from "@prisma/extension-optimize";
 
 declare global {
   var prisma: PrismaClient | undefined
 }
 
-const client = globalThis.prisma || (new PrismaClient().$extends(
-   withOptimize({ apiKey: process.env.OPTIMIZE_API_KEY as string })
- ) as PrismaClient)
+const client = globalThis.prisma || new PrismaClient()
 if (process.env.NODE_ENV !== "production") globalThis.prisma = client
 
 export default client
