33bedeae3e0c20ba01f23a63a92610cf064a22a8
oangsa <sukruangkul.aongsa@gmail.com>
2024-12-11 04:02:44 +0700

    [Fix] Fix parameters datatype in API

--- app/api/cron/notify/route.ts
+++ app/api/cron/notify/route.ts
@@ -1,8 +1,7 @@
-import type { NextApiRequest, NextApiResponse } from 'next'
 import prisma from '@/libs/prismadb'
-import { NextResponse } from 'next/server'
+import { NextRequest, NextResponse } from 'next/server'
 
-export async function GET(req: NextApiRequest, res: NextApiResponse ) {
+export async function GET(req: NextRequest, res: NextResponse ) {
 
    try {
       const companies = await prisma.company.findMany({
